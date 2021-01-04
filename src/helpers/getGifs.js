1c4c6e2c216c35460ed515bf65a3a96e6053fa55
Oscar Calle <gameplaysoscar@gmail.com>
2021-01-04 16:52:45 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+  const api_key = "0MMaOEI5RePCJZ2Y982VferPJ0jQ7K9G";
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=${api_key}`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+
+  return gifs;
+};
